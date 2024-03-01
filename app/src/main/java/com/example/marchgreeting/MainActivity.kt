7c7b5b67900d59930cf8718f79132437c486853a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-03-01 12:11:17 +0200

    Added Column to main Surface

--- app/src/main/java/com/example/marchgreeting/MainActivity.kt
+++ app/src/main/java/com/example/marchgreeting/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
